283f225e9cd189667d05b2a9e998ba8a6370901f
peterzhao <peter.qs.zhao@gmail.com>
2018-11-10 13:15:09 -0500

    integration test works

--- integration-tests/src/app.test.js
+++ integration-tests/src/app.test.js
@@ -4,9 +4,11 @@ const { expect } = require('chai')
 
 chai.use(chaiHttp)
 
+const host = process.env.host || 'localhost'
+
 describe('App', () => {
   it('should get 200', async () => {
-    const resp = await chai.request('http://localhost:8080')
+    const resp = await chai.request(`http://${host}:8080`)
       .get('/')
     expect(resp).to.have.status(200)
   })
